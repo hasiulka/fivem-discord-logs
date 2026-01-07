901848281bba99ce625e1b137dcd6f531c7b70d9
hasia <228372805+hasiulka@users.noreply.github.com>
2026-01-07 19:55:37 +0100

    Add fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'hasiulka'
+description 'Simple Discord webhook logger for FiveM'
+version '1.0.0'
+
+server_scripts {
+    'config.lua',
+    'server.lua'
+}
